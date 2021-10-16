36a53238b36ecf0b6349592b4b5e3cdbd65598f3
Samuel B. Powell <sbear.powell@gmail.com>
2021-10-16 23:15:10 +1000

    Work on database

--- db.cpp
+++ db.cpp
@@ -6,147 +6,281 @@ namespace imghash {
 	class Database::Impl {
 	public:
 		SQLite::Database db;
-		std::unique_ptr<SQLite::Statement> insert_files, insert_points, insert_vp, get_ps, get_vps, add_points_col, update_point;
+		
+		//used in insert()
+		std::unique_ptr<SQLite::Statement> insert_point, get_point_id_by_value, add_points_col, insert_file, update_file, get_file_by_path;
+		
+		//used in add_vp()
+		std::unique_ptr<SQLite::Statement> get_all_point_values, insert_vantage_point, get_vantage_points, update_point;
+		
+		
+		std::unique_ptr<SQLite::Statement> get_file_by_pointid;
+		std::unique_ptr<SQLite::Statement> get_count, increment_count;
 
-		Impl(const std::string& path)
-			: db(path, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE),
-			insert_files(nullptr)
-		{
-			//initialize database as necessary
-			SQLite::Transaction trans(db);
-			db.exec("CREATE TABLE IF NOT EXISTS files ("
-				"path TEXT PRIMARY KEY,"
-				"hash BLOB"
-				") WITHOUT ROWID;"
-			);
-			db.exec("CREATE INDEX IF NOT EXISTS "
-				"idx_files_hash ON files(hash);"
-			);
-			db.exec("CREATE TABLE IF NOT EXISTS points ("
-				"id INTEGER PRIMARY KEY,"
-				"hash BLOB"
-				");"
-			);
-			db.exec("CREATE TABLE IF NOT EXISTS vantage_points ("
-				"id INTEGER PRIMARY KEY,"
-				"hash BLOB"
-				");"
-			);
-			trans.commit();
-		}
+		std::vector<int64_t> vp_ids_;
 
 		std::unique_ptr<SQLite::Statement> make_stmt(const std::string& stmt)
 		{
 			return std::make_unique<SQLite::Statement>(db, stmt);
 		}
 
-		std::unique_ptr<SQLite::Statement> make_insert_points(const std::vector<int64_t>& ids)
+		void make_insert_point(const std::vector<int64_t>& vp_ids)
 		{
-			std::string stmt1 = "INSERT INTO points(hash";
-			std::string stmt2 = ") VALUE(?";
-			for (int64_t id : ids) {
-				auto id_str = std::to_string(id);
-				stmt1 += ", d" + id_str;
-				stmt2 += ", ?";
+			if (std::equal(vp_ids_.begin(), vp_ids_.end(), vp_ids.begin(), vp_ids.end())) {
+				return;
 			}
-			stmt2 += ");";
-			return make_stmt(stmt1 + stmt2);
+			else {
+				// INSERT INTO points(value, d0, d1, ...) VALUES (?, ?, ?, ...) RETURNING id;
+				std::string stmt1 = "INSERT INTO points(value";
+				std::string stmt2 = ") VALUES (?";
+				for (int64_t id : vp_ids) {
+					auto id_str = std::to_string(id);
+					stmt1 += ", d" + id_str;
+					stmt2 += ", ?";
+				}
+				stmt2 += ") RETURNING id;";
+				insert_point = make_stmt(stmt1 + stmt2);
+				vp_ids_ = vp_ids;
+			}
 		}
 
-		Hash::hash_type get_hash(SQLite::Column& col) {
+		Impl(const std::string& path)
+			: db(path, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE)
+		{
+			//initialize database as necessary
+			SQLite::Transaction trans(db);
+            db.exec(
+				"CREATE TABLE IF NOT EXISTS counts ("
+					"id INTEGER PRIMARY KEY,"
+					"points INTEGER,"
+					"vantange_points INTEGER,"
+					"files INTEGER"
+				");"
+
+				"CREATE TABLE IF NOT EXISTS points ("
+					"id INTEGER PRIMARY KEY,"
+					"value BLOB"
+				");"
+
+				"CREATE UNIQUE INDEX IF NOT EXISTS idx_points_value ON points(value);"
+
+				"CREATE TABLE IF NOT EXISTS vantage_points ("
+					"id INTEGER PRIMARY KEY,"
+					"value BLOB UNIQUE"
+				");"
+
+				"CREATE TABLE IF NOT EXISTS files ("
+					"path TEXT PRIMARY KEY,"
+					"pointid INTEGER,"
+					"FOREIGN KEY(pointid) REFERENCES points(id)"
+				") WITHOUT ROWID;"
+
+				"CREATE INDEX IF NOT EXISTS"
+					"idx_files_point ON files(pointid);"
+				");"
+			);
+			if (db.execAndGet("SELECT COUNT(1) FROM counts;").getInt64() == 0)
+			{
+				auto num_points = db.execAndGet("SELECT COUNT(1) FROM points;").getInt64();
+				auto num_vantage_points = db.execAndGet("SELECT COUNT(1) FROM vantage_points;").getInt64();
+				auto num_files = db.execAndGet("SELECT COUNT(2) FROM files;").getInt64();
+				auto insert_counts = make_stmt("INSERT INTO counts(points,vantage_points,files) VALUES(?, ?, ?);");
+				insert_counts->bind(1, num_points);
+				insert_counts->bind(2, num_vantage_points);
+				insert_counts->bind(3, num_files);
+				insert_counts->exec();
+			}
+
+			std::vector<int64_t> vp_ids;
+
+			SQLite::Statement get_vp_ids(db, "SELECT id FROM vantage_points ORDER BY id ASC;");
+			while (get_vp_ids.executeStep()) {
+				vp_ids.push_back(get_vp_ids.getColumn(0).getInt64());
+			}
+
+			trans.commit();
+
+			//precompile statements
+			make_insert_point(vp_ids);
+			get_all_point_values = make_stmt("SELECT id, value FROM points;");
+			get_point_id_by_value = make_stmt("SELECT id FROM points WHERE value = $value;");
+			update_point = make_stmt("UPDATE points SET $col = $val WHERE id = $id;");
+
+			insert_vantage_point = make_stmt("INSERT INTO vantage_points(value) VALUES($value) RETURNING id;");
+			get_vantage_points = make_stmt("SELECT id, value FROM vantage_points ORDER BY id ASC;");
+			add_points_col = make_stmt("ALTER TABLE points ADD COLUMN $col INTEGER DEFAULT 0xFFFFFFFF; CREATE INDEX $idx ON points($col);");
+
+			insert_file = make_stmt("INSERT INTO files(path, pointid) VALUES($path, $pointid);");
+			update_file = make_stmt("UPDATE files SET pointid = $pointid WHERE path = $path;");
+			get_file_by_path = make_stmt("SELECT pointid FROM files where path = $path;");
+			get_file_by_pointid = make_stmt("SELECT path FROM files WHERE pointid = $pointid;");
+			
+			get_count = make_stmt("SELECT points, vantage_points, files FROM counts WHERE id = 1;");
+			increment_count = make_stmt("UPDATE counts SET $col = $col + 1 WHERE id = 1;");
+		}
+
+		point_type get_point_value(SQLite::Column& col) {
 			size_t n = col.getBytes();
 			const uint8_t* data = static_cast<const uint8_t*>(col.getBlob());
-			return Hash::hash_type(data, data + n);
+			return point_type(data, data + n);
 		}
 
-		void insert(const Hash::hash_type& p_hash, const std::string& path)
+		void bind_point_value(SQLite::Statement& stmt, const char* n, const point_type& p)
 		{
-			//Calculate distances to each vantage point
-			if (!get_vps)
-				get_vps = make_stmt("SELECT * FROM vantage_points ORDER BY id ASC;");
-			
-			get_vps->reset();
-			std::vector<int64_t> ids;
-			std::vector<uint32_t> dists;
-			while (get_vps->executeStep()) {
-				auto id = get_vps->getColumn(0).getInt64(); //vantage point id
-				ids.push_back(id);
-				//copy the blob into a hash_type
-				//TODO: use span to avoid copies
-				auto vp_hash = get_hash(get_vps->getColumn(1));
+			stmt.bind(n, p.data(), p.size());
+		}
 
-				uint32_t d = Hash::hamming_distance(vp_hash, p_hash);
-				dists.push_back(d);
-			}
-			//Prepare insertion into files
-			if (!insert_files)
-				insert_files = make_stmt("INSERT OR REPLACE INTO files VALUES(:path, :hash);");
+		void bind_point_value(SQLite::Statement& stmt, const int n, const point_type& p)
+		{
+			stmt.bind(n, p.data(), p.size());
+		}
 
-			insert_files->reset();
-			insert_files->bind("path", path);
-			insert_files->bind("hash", p_hash.data(), p_hash.size());
-			//Prepare insertion into points
-			if (!insert_points)
-				insert_points = make_insert_points(ids);
-			insert_points->reset();
-			insert_points->bind(1, p_hash.data(), p_hash.size());
-			for (size_t i = 0; i < dists.size(); ++i) {
-				insert_points->bind(i + 2, dists[i]); //the first parameter has index 1, so these start at 2
-			}
+		uint32_t get_distance(const point_type& p1, const point_type& p2)
+		{
+			return Hash::distance(p1, p2);
+		}
 
-			//Perform the insertions
+		void insert(const point_type& p_value, const std::string& path)
+		{
 			SQLite::Transaction trans(db);
-			insert_files->exec();
-			insert_points->exec();
+
+			//is the hash already in the database?
+			bind_point_value(*get_point_id_by_value, "value", p_value);
+			int64_t point_id = -1;
+			get_point_id_by_value->reset();
+			if (get_point_id_by_value->executeStep()) {
+				//yes
+				point_id = get_point_id_by_value->getColumn(0).getInt64();
+			}
+			else {
+				//no: we need to add the point
+
+				//Calculate distances to each vantage point
+				std::vector<int64_t> vp_ids;
+				std::vector<uint32_t> dists;
+
+				get_vantage_points->reset();
+				while (get_vantage_points->executeStep()) {
+					auto id = get_vantage_points->getColumn(0).getInt64(); //vantage point id
+					vp_ids.push_back(id);
+					//copy the blob into a hash_type
+					auto vp_value = get_point_value(get_vantage_points->getColumn(1)); //TODO: use span to avoid copies
+					uint32_t d = get_distance(vp_value, p_value);
+					dists.push_back(d);
+				}
+				
+				make_insert_point(vp_ids); //update the insert_point statement if vp_ids changed
+				bind_point_value(*insert_point, 1, p_value);
+				for (size_t i = 0; i < dists.size(); ++i) {
+					insert_point->bind(i + 2, dists[i]); //the first parameter has index 1, so these start at 2
+				}
+				insert_point->reset();
+				if (insert_point->executeStep()) {
+					point_id = insert_point->getColumn(0).getInt64();
+					
+					increment_count->bind("col", "points");
+					increment_count->reset();
+					increment_count->exec();
+				}
+				else {
+					throw std::runtime_error("Error inserting point");
+				}
+				//done adding the hash
+			}
+
+			//is the file already in the database?
+			get_file_by_path->bind("path", path);
+			get_file_by_path->reset();
+			if (get_file_by_path->executeStep()) {
+				//yes, does the pointid match?
+				auto old_id = get_file_by_path->getColumn(0).getInt64();
+				if (old_id != point_id) {
+					//no -- update it
+					update_file->bind("path", path);
+					update_file->bind("pointid", point_id);
+					update_file->reset();
+					update_file->exec();
+
+				}
+			}
+			else {
+				//no: we need to insert the file
+				insert_file->bind("path", path);
+				insert_file->bind("pointid", point_id);
+				insert_file->reset();
+				insert_file->exec();
+
+				increment_count->bind("col", "files");
+				increment_count->reset();
+				increment_count->exec();
+			}
+			
+			//done
 			trans.commit();
 		}
 
-		void add_vp(const Hash::hash_type& vp_hash)
+		void add_vp(const point_type& vp_value)
 		{
-			//Prepare insertion into vantage_points
-			if (!insert_vp)
-				insert_vp = make_stmt("INSERT INTO vantage_points(hash) VALUES(:hash) RETURNING id;");
-
-			if (!add_points_col)
-				add_points_col = make_stmt("ALTER TABLE points ADD COLUMN :col INTEGER DEFAULT 0xFFFFFFFF;"
-					"CREATE INDEX :idx ON points(:col);");
-
-			if (!get_ps)
-				get_ps = make_stmt("SELECT id,hash FROM points;");
-
-			if (!update_point)
-				update_point = make_stmt("UPDATE points SET :col = :val WHERE id = :id");
-
-			insert_vp->reset();
-			add_points_col->reset();
-			get_ps->reset();
-			update_point->reset();
-
-			insert_vp->bind("hash", vp_hash.data(), vp_hash.size());
-			//we need to get the new vantage point's ID before we can bind add_points_col's parameters
-			// so it will need to happen within the transaction
 			SQLite::Transaction trans(db);
-			if (!insert_vp->executeStep()) {
+			
+			bind_point_value(*insert_vantage_point, "value", vp_value);
+			int64_t vp_id = -1;
+			insert_vantage_point->reset();
+			if (insert_vantage_point->executeStep()) {
+				vp_id = insert_vantage_point->getColumn(0).getInt64();
+
+				increment_count->bind("col", "vantage_points");
+				increment_count->reset();
+				increment_count->exec();
+			}
+			else {
 				throw std::runtime_error("Error inserting vantage point");
 			}
-			uint64_t id = insert_vp->getColumn(0).getInt64();
-			std::string col_name = "d" + std::to_string(id);
+			std::string col_name = "d" + std::to_string(vp_id);
+
 			add_points_col->bind("col", col_name);
 			add_points_col->bind("idx", "idx_" + col_name);
-			
+			add_points_col->reset();
 			add_points_col->exec();
 			
-			//the database is almost in a usable state again, but we need to compute all of the distances
-			// but it won't return correct results for the newest vantage point yet
+			// we need to compute the distance from the new vantage point to all of the existing points
 			update_point->bind("col", col_name);
-			while (get_ps->executeStep()) {
-				auto id = get_ps->getColumn(0).getInt64();
-				auto p_hash = get_hash(get_ps->getColumn(1));
+
+			get_all_point_values->reset();
+			while (get_all_point_values->executeStep()) {
+				auto id = get_all_point_values->getColumn(0).getInt64();
+				auto p_value = get_point_value(get_all_point_values->getColumn(1));
 				
-				uint32_t d = Hash::hamming_distance(vp_hash, p_hash);
+				uint32_t d = get_distance(vp_value, p_value);
 				update_point->bind("id", id);
 				update_point->bind("val", d);
+				update_point->reset();
 				update_point->exec();
+			}
+
+			trans.commit();
+		}
+	
+		point_type find_vantage_point(size_t sample_size)
+		{
+			SQLite::Transaction trans(db);
+
+			// do we have any vantage points yet?
+			int64_t num_vantage_points = -1;
+			get_count->reset();
+			if (get_count->executeStep()) {
+				num_vantage_points = get_count->getColumn(1).getInt64();
+			}
+			else {
+				throw std::runtime_error("Error reading number of vantage points");
+			}
+
+			if (num_vantage_points > 0) {
+				//we need to find a point that's far from all of the existing vantage points
+
+			}
+			else {
+				//we need to find a point that's far from all other points
 			}
 
 			trans.commit();
@@ -185,9 +319,9 @@ namespace imghash {
 	}
 
 	//Find a point that would make a good vantage point
-	Hash::hash_type Database::find_vantage_point()
+	Hash::hash_type Database::find_vantage_point(size_t sample_size)
 	{
-
+		return impl->find_vantage_point(sample_size);
 	}
 	
 }
--- db.h
+++ db.h
@@ -13,6 +13,9 @@ namespace imghash {
 		class Impl;
 		std::unique_ptr<Impl> impl;
 	public:
+		
+		using point_type = Hash::hash_type;
+
 		//Open the database
 		Database(const std::string& path);
 
@@ -20,15 +23,27 @@ namespace imghash {
 		~Database();
 
 		//Add a file
-		void insert(const Hash::hash_type& hash, const std::string& name);
+		void insert(const point_type& hash, const std::string& name);
 
 		//Find similar images
-		void query(const Hash::hash_type& hash, unsigned int dist);
+		void query(const point_type& hash, unsigned int dist);
 
 		//Add a vantage point for querying
-		void add_vantage_point(const Hash::hash_type& hash);
+		// NB this is expensive: it computes (and stores) the distance from the new
+		//  vantage point to every point already in the database
+		void add_vantage_point(const point_type& hash);
 
 		//Find a point that would make a good vantage point
-		Hash::hash_type find_vantage_point();
+		// this is a point that is far from all existing vantage points
+		// if there are no vantage points yet, the best is a point that is distant from most other points
+		//  this will be determined by randomly subsampling 'sample_size' candidate vantage points and computing 
+		//  the distance from each to another random subsample of "friends". The candidate with the most distant
+		//  friends will be returned. That is, each candidate's friends will be sorted from nearest to furthest
+		//  and ranked relative to the previous best candidate. The candidate with more friends that are further
+		//  away than the other wins.
+		//  NB that this algorithm is expensive:
+		//    O(sample_size*sample_size) distance computations
+		//    + O(sample_size*sample_size*sample_size*log(sample_size)) distance comparisons for the ranking
+		point_type find_vantage_point(size_t sample_size = 25);
 	};
 }
